74eeabd41be4703f7320a958fce273d4861618da
felonzo1 <olatubora1996@gmail.com>
2020-09-17 00:00:20 +0100

    Sport webpage

--- assets/js/main.js
+++ assets/js/main.js
@@ -57,6 +57,29 @@
 
 }(jQuery));
 
+// var i = [0];
+// var images = [];
+// var time = 3000;
+
+// images[0] = './assets/images/player/player1.jpg';
+// images[1] = './assets/images/player/player2.jpg';
+// images[2] = './assets/images/player/player3.jpg';
+// images[3] = './assets/images/player/player4.jpg';
+
+// function changeImg() {
+//     document.slide.src = images[i]
+
+//     if (i < images.length - 1) {
+//         i++
+//     } else {
+//         i = 0
+//     }
+
+//     setTimeout("changeImg()", time);
+// }
+
+// window.onload = changeImg;
+
 let year = document.querySelector('.year').innerHTML = new Date().getFullYear();
 let month = document.querySelector('.month').innerHTML = new Date().getMonth();
-let date = document.querySelector('.dtate').innerHTML = new Date().getDate();
+let date = document.querySelector('.date').innerHTML = new Date().getDate();
